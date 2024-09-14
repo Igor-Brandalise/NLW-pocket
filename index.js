5910974a89b06de5512fae84283f943816fd6bdf
Igor Brandalise <brandaliseigor@gmail.com>
2024-09-14 18:35:21 -0300

    add metas abertas

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const listarMetas = async () => {
 };
 
 const metasRealizadas = async () => {
-  const metasRealizadas = metas.filter((meta) => {
+  const realizadas = metas.filter((meta) => {
     return meta.checked;
   });
 
@@ -60,8 +60,24 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: "metas realizadas",
+    message: "metas realizadas" + realizads.length,
     choices: [...realizadas],
+  });
+};
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true;
+  });
+
+  if (abertas.length == 0) {
+    console.log("Não existem metas abertas! :) ");
+    ruturn;
+  }
+
+  await select({
+    message: "Metas Abertas" + abertas.length,
+    choices: [...abertas],
   });
 };
 
@@ -79,6 +95,14 @@ const start = async () => {
           value: "listar",
         },
         {
+          name: "Metas realizadas",
+          value: "realizadas",
+        },
+        {
+          name: "Metas abertas",
+          value: "abertas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -92,6 +116,12 @@ const start = async () => {
         break;
       case "listar":
         await listarMetas();
+        break;
+      case "realizadas":
+        await metasRealizadas();
+        break;
+      case "abertas":
+        await metasAbertas();
         break;
       case "sair":
         console.log("Até a próxima!");
