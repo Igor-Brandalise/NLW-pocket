035208b7d70346da2a9650b871c8ee866f690ac6
Igor Brandalise <brandaliseigor@gmail.com>
2024-09-14 14:49:19 -0300

    criação menu

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-const start = () => {
-    
-};
+const { select, input, checkbox } = require('@inquirer/prompts')
+
+const start = async () => {
+    while(true){
+        
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        switch(opcao) {
+            case "cadastrar":
+                await cadastrarMeta()
+                console.log(metas)
+                break
+            case "listar":
+                await listarMetas()
+                break
+            case "sair":
+                console.log('Até a próxima!')
+                return
+        }
+    }
+}
 
 start();
